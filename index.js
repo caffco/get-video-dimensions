f335442154a7401f3e379562be2cdbca15cc2fca
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2015-08-03 08:28:28 +0200

    Rotation will be taken into account and possibly swap width and height

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+var exec = require( 'mz/child_process' ).execFile;
+var assert = require( 'assert' );
 
-var exec = require('mz/child_process').execFile;
-var assert = require('assert');
-
-module.exports = function (filename) {
-  return exec('ffprobe', [
-    '-v', 'error',
-    '-of', 'flat=s=_',
-    '-select_streams', 'v:0',
-    '-show_entries', 'stream=height,width',
-    filename
-  ]).then(function (out) {
-    var stdout = out[0].toString('utf8');
-    var width = /width=(\d+)/.exec(stdout);
-    var height = /height=(\d+)/.exec(stdout);
-    assert(width && height, 'No dimensions found!');
-    return {
-      width: parseInt(width[1]),
-      height: parseInt(height[1]),
-    };
-  });
-}
+module.exports = function ( filename ) {
+	return exec( 'ffprobe', [
+		'-v', 'error',
+		'-of', 'flat=s=_',
+		'-select_streams', 'v:0',
+		'-show_streams',
+		filename
+	] ).then( function ( out ) {
+		var stdout = out[ 0 ].toString( 'utf8' );
+		var width = /width=(\d+)/.exec( stdout );
+		var height = /height=(\d+)/.exec( stdout );
+		assert( width && height, 'No dimensions found!' );
+		var rotate = /tags_rotate=(\d+)/.exec( stdout );
+		if ( rotate !== undefined ) {
+			rotate = parseInt( rotate[ 1 ] );
+			if ( rotate === 90 || rotate === -90 ) {
+				return {
+					width: parseInt( height[ 1 ] ),
+					height: parseInt( width[ 1 ] ),
+				};
+			}
+		}
+		return {
+			width: parseInt( width[ 1 ] ),
+			height: parseInt( height[ 1 ] ),
+		};
+	} );
+};
--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
+var assert = require( 'assert' );
 
-var assert = require('assert');
+var getDimensions = require( '..' );
 
-var getDimensions = require('..');
-
-it('should get dimensions', function () {
-  return getDimensions('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4').then(function (size) {
-    assert.equal(size.width, 640);
-    assert.equal(size.height, 360);
-  });
-})
+it( 'should get dimensions', function () {
+	return getDimensions( 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' )
+		.then( function ( size ) {
+			assert.equal( size.width, 640 );
+			assert.equal( size.height, 360 );
+		} );
+} )
